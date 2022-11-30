905eba572935c8085732feda266c0da46f8f9ed6
KGTH <kgthcruz94@gmail.com>
2022-11-30 17:33:36 +0000

    navbar

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,7 +3,7 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => (
-	<div className="text-center mt-5">
+	<div className=" text-center mt-5">
 		<h1>Hello Rigo!</h1>
 		<p>
 			<img src={rigoImage} />
