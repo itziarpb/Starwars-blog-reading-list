6763837423c40b984c7a0fa20613a93c5b4300eb
KGTH <kgthcruz94@gmail.com>
2022-12-01 20:52:56 +0000

    se actualiza

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,38 @@
-import React from "react";
+import React,{useEffect, useState}from "react";
+import Cards  from "../component/cards";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
+ 
+
+const [characters, setCharacters]=useState([])
+
+
+/*useEffect (()=>{
+  fetch('https://www.swapi.tech/api/people')
+    .then(response =()=>{
+    return response.json()
+    .then(response)=()=>{
+        setCharacters(response)
+    }
+})
+
+},[])
+*/
 export const Home = () => (
-	<div className=" text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+	
+	<div className=" text-center mt-5">{
+		characters.map=(value, index)=>{
+			return <Cards url={value.url} uid={value.uid} title={value.title} />
+
+
+		}
+
+	}
+
+		
+		
+		
 	</div>
+	
 );
