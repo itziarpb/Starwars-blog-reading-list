6b961a3dd729a2175f15ab1d554ee9881d3389d8
KGTH <kgthcruz94@gmail.com>
2022-12-02 15:06:30 +0000

    pendiente del api

--- src/js/component/cards.js
+++ src/js/component/cards.js
@@ -1,31 +1,31 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
+import Products from "./products";
 
+          const Cards =()=>{
+            const [characters, setCharacters]=useState([])
+
+            useEffect (()=>{
+              fetch('https://www.swapi.tech/api/people/')
+              .then(response=>{
+                return response.json
+                .then (response=>{
+                  setCharacters(response.results)
+                })
+              })
+
+          },[])  
   
-  const Cards =(props)=>{
-
     return (
-   <div className="card row" >
+      <div className="card row" >
+        <Products  />
     
-    <div className="col-12 col-md-3">
-        
-  <img src={props.url} className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">{props.title}</h5>
-    <p  className={props.uid}></p>
-    <p className="card-text"></p>
-    <a href="#" className="btn btn-primary">Learn More!</a>
-    <a href="#" className="btn btn-primary"></a>
-    </div>
-    </div>
-  
 </div>
 
 
     )
+  }
 
-
-}
-
+  
 export default Cards
 
 
